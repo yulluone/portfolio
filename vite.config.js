053ba2287402151f39a0797c0aa6bba2b809694d
YulluOne <38428314+joshuamwo@users.noreply.github.com>
2023-05-17 13:09:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/work/dev-potfolio-starter/",
   plugins: [vue()],
   test: {
     files: ["src/**/*.spec.js"],
